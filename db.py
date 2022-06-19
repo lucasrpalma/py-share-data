0313b1996ba4a64504238826be298a6ec7c08c86
lucasrpalma <palma.lucas@gmail.com>
2022-06-19 11:16:08 -0300

    Inserting users needed for this exercise

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 ''' Database related functions '''
 
+from multiprocessing.spawn import prepare
 import mysql.connector
 from mysql.connector import errorcode
 import sql.tables
+import sql.users
 
 # Hardcoded DB credentials, in production use Vault or some other service
 DB_USER = "root"
@@ -20,6 +22,7 @@ def __init_db():
     cursor.execute("DROP DATABASE IF EXISTS storage")
     cursor.execute("CREATE DATABASE storage")
     cursor.close()
+    mydb.close()
 
 def __init_tables():
     ''' Create the default tables '''
@@ -42,8 +45,31 @@ def __init_tables():
         else:
             print("OK")
     cursor.close()
+    mydb.close()
+
+def __init_users():
+    ''' Create the default users '''
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database="storage"
+    )
+    cursor = mydb.cursor()
+    for key, value in sql.users.USERS.items():
+        try:
+            print(f"Creating user {key}: ", end='')
+            cursor.execute(sql.users.ADD_USER, value)
+        except mysql.connector.Error as err:
+            print('Error: ' + err.msg)
+        else:
+            print("OK")
+    mydb.commit()
+    cursor.close()
+    mydb.close()
 
 def init():
     ''' Initilization function for the DB '''
     __init_db()
     __init_tables()
+    __init_users()
